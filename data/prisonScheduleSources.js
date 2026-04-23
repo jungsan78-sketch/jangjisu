a02c3de31d74082d269ac556464bef1e1f3a9012
jungsan78-sketch <jungsan78@gmail.com>
2026-04-23 21:01:06 +0900

    Split prison schedule sources into separate module

---
+++ data/prisonScheduleSources.js
@@ -0,0 +1,17 @@
+export const PRISON_SCHEDULE_SOURCES = [
+  {
+    key: 'jangjisu',
+    member: '장지수',
+    endpoint: '/api/schedule',
+  },
+  {
+    key: 'youoneul',
+    member: '유오늘',
+    endpoint: '/api/youoneul-schedule',
+  },
+  {
+    key: 'rinring',
+    member: '린링',
+    endpoint: '/api/rinring-schedule',
+  },
+];
