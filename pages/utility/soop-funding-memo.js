b1bc578c72a91278b0a593bed13c280a8e47884c
jungsan78-sketch <jungsan78@gmail.com>
2026-04-25 18:22:52 +0900

    Use stable SOOP SDK memo with dedupe

--- pages/utility/soop-funding-memo.js
+++ pages/utility/soop-funding-memo.js
@@ -1,13 +1,8 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-const SoopOfficialChatSdkMemo = dynamic(
-  () => import('../../components/utility/SoopOfficialChatSdkMemo'),
-  { ssr: false }
-);
-
-const SoopSdkEventProbe = dynamic(
-  () => import('../../components/utility/SoopSdkEventProbe'),
+const SoopOfficialChatSdkMemoStable = dynamic(
+  () => import('../../components/utility/SoopOfficialChatSdkMemoStable'),
   { ssr: false }
 );
 
@@ -18,12 +13,7 @@ export default function SoopFundingMemoPage() {
         <title>장지수용소 펀딩 자동메모장 | 유틸리티</title>
         <meta name="description" content="SOOP 공식 Chat SDK로 별풍선과 채팅을 매칭하는 핀볼용 자동메모장" />
       </Head>
-      <SoopOfficialChatSdkMemo />
-      <div className="bg-[#05070c] px-5 pb-8 lg:px-8">
-        <div className="mx-auto max-w-[1780px]">
-          <SoopSdkEventProbe />
-        </div>
-      </div>
+      <SoopOfficialChatSdkMemoStable />
     </>
   );
 }
