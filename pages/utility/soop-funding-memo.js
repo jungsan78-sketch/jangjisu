3a73588e17f976e3bb0eefa00cff3e5b5c727280
jungsan78-sketch <jungsan78@gmail.com>
2026-04-25 15:14:04 +0900

    Show SOOP collector bookmarklet guide

--- pages/utility/soop-funding-memo.js
+++ pages/utility/soop-funding-memo.js
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import SoopFundingBookmarkletGuide from '../../components/utility/SoopFundingBookmarkletGuide';
 
 const SoopFundingMemoPersistentSafe = dynamic(
   () => import('../../components/utility/SoopFundingMemoPersistentSafe'),
@@ -13,6 +14,7 @@ export default function SoopFundingMemoPage() {
         <title>장지수용소 펀딩 자동메모장 | 유틸리티</title>
         <meta name="description" content="SOOP 펀딩 데이터를 핀볼용 복붙 메모로 변환하는 유틸리티" />
       </Head>
+      <SoopFundingBookmarkletGuide />
       <SoopFundingMemoPersistentSafe />
     </>
   );
