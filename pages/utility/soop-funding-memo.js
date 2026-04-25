f799fb58c68d14af509f6b66535054ed829d0616
jungsan78-sketch <jungsan78@gmail.com>
2026-04-25 15:06:47 +0900

    Fix funding memo prerender error

--- pages/utility/soop-funding-memo.js
+++ pages/utility/soop-funding-memo.js
@@ -1,5 +1,10 @@
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import SoopFundingMemoPersistent from '../../components/utility/SoopFundingMemoPersistent';
+
+const SoopFundingMemoPersistentSafe = dynamic(
+  () => import('../../components/utility/SoopFundingMemoPersistentSafe'),
+  { ssr: false }
+);
 
 export default function SoopFundingMemoPage() {
   return (
@@ -8,7 +13,7 @@ export default function SoopFundingMemoPage() {
         <title>장지수용소 펀딩 자동메모장 | 유틸리티</title>
         <meta name="description" content="SOOP 펀딩 데이터를 핀볼용 복붙 메모로 변환하는 유틸리티" />
       </Head>
-      <SoopFundingMemoPersistent />
+      <SoopFundingMemoPersistentSafe />
     </>
   );
 }
