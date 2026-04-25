a38479b19dc1bec67d5e31de6af06ba33555b98d
jungsan78-sketch <jungsan78@gmail.com>
2026-04-25 15:50:24 +0900

    Use clean SOOP funding memo without collector UI

--- pages/utility/soop-funding-memo.js
+++ pages/utility/soop-funding-memo.js
@@ -1,8 +1,8 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-const SoopFundingMemoPersistentSafe = dynamic(
-  () => import('../../components/utility/SoopFundingMemoPersistentSafe'),
+const SoopFundingMemoClean = dynamic(
+  () => import('../../components/utility/SoopFundingMemoClean'),
   { ssr: false }
 );
 
@@ -13,7 +13,7 @@ export default function SoopFundingMemoPage() {
         <title>장지수용소 펀딩 자동메모장 | 유틸리티</title>
         <meta name="description" content="SOOP 펀딩 데이터를 핀볼용 복붙 메모로 변환하는 유틸리티" />
       </Head>
-      <SoopFundingMemoPersistentSafe />
+      <SoopFundingMemoClean />
     </>
   );
 }
