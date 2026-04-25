d9ec849ca478952a62ccbf413fe69d3dc31735f0
jungsan78-sketch <jungsan78@gmail.com>
2026-04-25 20:00:20 +0900

    Combine live status hydrator app mount

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import NewYoutubeDomToast from '../components/NewYoutubeDomToast';
+import PrisonLiveStatusHydrator from '../components/PrisonLiveStatusHydrator';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
       <NewYoutubeDomToast />
+      <PrisonLiveStatusHydrator />
     </>
   );
 }
