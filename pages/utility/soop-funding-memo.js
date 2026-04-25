df4ad8c0e9a51785d26ca9549f223c776de18d31
jungsan78-sketch <jungsan78@gmail.com>
2026-04-25 15:01:23 +0900

    Use persistent SOOP funding memo component

--- pages/utility/soop-funding-memo.js
+++ pages/utility/soop-funding-memo.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import SoopFundingMemoCollector from '../../components/utility/SoopFundingMemoCollector';
+import SoopFundingMemoPersistent from '../../components/utility/SoopFundingMemoPersistent';
 
 export default function SoopFundingMemoPage() {
   return (
@@ -8,7 +8,7 @@ export default function SoopFundingMemoPage() {
         <title>장지수용소 펀딩 자동메모장 | 유틸리티</title>
         <meta name="description" content="SOOP 펀딩 데이터를 핀볼용 복붙 메모로 변환하는 유틸리티" />
       </Head>
-      <SoopFundingMemoCollector />
+      <SoopFundingMemoPersistent />
     </>
   );
 }
