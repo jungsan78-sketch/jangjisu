18809a1452a0a48b70937f0a4505c91154536fd2
jungsan78-sketch <jungsan78@gmail.com>
2026-04-28 08:04:41 +0900

    Add schedule source links and Guwol

--- data/prisonScheduleSources.js
+++ data/prisonScheduleSources.js
@@ -3,25 +3,36 @@ export const PRISON_SCHEDULE_SOURCES = [
     key: 'jangjisu',
     member: '장지수',
     endpoint: '/api/schedule',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/1b1-p5I4CGEdLwI7XxyyAMDtEjmR9lEzOtoL-vAwo5PM/edit',
   },
   {
     key: 'youoneul',
     member: '유오늘',
     endpoint: '/api/youoneul-schedule',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/1OLJnia52yhNXvbTlt273EqO3kIggUy1e-uZso60eHwo/edit?gid=399310385#gid=399310385',
+  },
+  {
+    key: 'guwol',
+    member: '구월이',
+    endpoint: '/api/guwol-schedule',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/1J0H1eHRB05ojAW3kqHrQBoMU68DjJV4SgRViwszyZBs/edit?gid=1645820954#gid=1645820954',
   },
   {
     key: 'rinring',
     member: '린링',
     endpoint: '/api/rinring-schedule',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/1qu7DXG99c9WbR5g-t1HL2BU_bFlqhxwN45tscolZ_U0/edit?gid=242454512#gid=242454512',
   },
   {
     key: 'popo',
     member: '포포',
     endpoint: '/api/popo-schedule',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/1bZwCSbYQ_8g-SmDFf0cWo6Nh6Ysyo1pCmrRhNXJGzpI/edit?gid=167079174#gid=167079174',
   },
   {
     key: 'ddikku',
     member: '띠꾸',
     endpoint: '/api/ddikku-schedule',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/165CKJlUjtZW9NYzHRPZuHDxNKLETpgYt48cxrMKuUGc/edit?gid=1059909393#gid=1059909393',
   },
 ];
