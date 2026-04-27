6c765e7a40596b3a1c5142176ad48deda1d7140a
jungsan78-sketch <jungsan78@gmail.com>
2026-04-27 14:51:24 +0900

    Hide legacy prison member section on v2 page

--- pages/jangjisu-prison-v2.js
+++ pages/jangjisu-prison-v2.js
@@ -8,6 +8,11 @@ export default function JangjisuPrisonV2Page() {
         <title>장지수용소 모드 | 장지수용소 팬메이드</title>
         <meta name="description" content="장지수용소 팬메이드 서브사이트" />
       </Head>
+      <style jsx global>{`
+        body:has(img[src="/jangjisu-prison-hero.png"]) section#members:not(.sou-member-live-section) {
+          display: none !important;
+        }
+      `}</style>
       <div className="min-h-screen bg-[#05070c] text-white">
         <PrisonPageContent />
       </div>
