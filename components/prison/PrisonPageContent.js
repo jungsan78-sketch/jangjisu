9b9559bbed58aab08bdbd584a23b26f9a2e8b592
jungsan78-sketch <jungsan78@gmail.com>
2026-04-28 10:52:21 +0900

    Insert shorts hall before member posts

--- components/prison/PrisonPageContent.js
+++ components/prison/PrisonPageContent.js
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from 'react';
 import { createPortal } from 'react-dom';
 import CalendarPreview from './CalendarPreview';
 import RecentYoutubeSection from './RecentYoutubeSection';
+import ShortsHallOfFame from './ShortsHallOfFame';
 import { PrisonMemberLiveGridContent } from '../PrisonMemberLiveGrid';
 import { ALL_PRISON_MEMBERS } from '../../data/prisonMembers';
 
@@ -287,6 +288,7 @@ export default function PrisonPageContent() {
               </section>
 
               <div id="members" className="scroll-mt-6 w-full max-w-none overflow-x-hidden">
+                <ShortsHallOfFame />
                 <PrisonMemberLiveGridContent />
               </div>
               <CalendarPreview />
