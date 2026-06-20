4e49bb49e4cc145c1d1700c488a86feda74c8254
jungsan78-sketch <jungsan78@gmail.com>
2026-06-21 00:22:04 +0900

    Redirect Jisu Dream links to external domain

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+
 export default function SiteFooter({ className = '' }) {
+  useEffect(() => {
+    const handleClick = (event) => {
+      const link = event.target?.closest?.('a[href="/jisu-dream"], a[href^="/jisu-dream#"]');
+      if (!link) return;
+      event.preventDefault();
+      window.location.href = 'https://www.jisoodream.xyz/';
+    };
+
+    document.addEventListener('click', handleClick, true);
+    return () => document.removeEventListener('click', handleClick, true);
+  }, []);
+
   return (
     <footer className={`px-5 pb-8 pt-12 text-center lg:px-8 ${className}`}>
       <div className="mx-auto max-w-7xl border-t border-white/[0.06] pt-6">
