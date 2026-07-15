c7a3f433f92ebb633e5e8e6dfff967a196dd8718
jungsan78-sketch <jungsan78@gmail.com>
2026-07-15 13:14:42 +0900

    Rename broadcast calendar page title

--- pages/jangjisu-prison/broadcast-summary.js
+++ pages/jangjisu-prison/broadcast-summary.js
@@ -6,8 +6,8 @@ export default function PrisonBroadcastSummaryPage() {
   return (
     <>
       <Head>
-        <title>이번 달 다시보기 달력 | 장지수용소</title>
-        <meta name="description" content="장지수용소 멤버들의 이번 달 다시보기 방송시간과 제목을 달력으로 보는 페이지" />
+        <title>다시보기 달력 | 장지수용소</title>
+        <meta name="description" content="장지수용소 멤버들의 다시보기 방송시간과 제목을 달력으로 보는 페이지" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <PrisonPageChrome wide>
