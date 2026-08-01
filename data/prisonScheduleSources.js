d4f417a69881f210a1d75b04431f190e4aa42bf6
jungsan78-sketch <jungsan78@gmail.com>
2026-08-01 16:18:07 +0900

    Add Youoneul schedule source

--- data/prisonScheduleSources.js
+++ data/prisonScheduleSources.js
@@ -17,6 +17,12 @@ export const PRISON_SCHEDULE_SOURCES = [
     endpoint: '/api/prison-schedule?key=linling',
     sourceUrl: 'https://docs.google.com/spreadsheets/d/1qu7DXG99c9WbR5g-t1HL2BU_bFlqhxwN45tscolZ_U0/edit?gid=1838232194#gid=1838232194',
   },
+  {
+    key: 'youoneul',
+    member: '유오늘',
+    endpoint: '/api/prison-schedule?key=youoneul',
+    sourceUrl: 'https://docs.google.com/spreadsheets/d/1OLJnia52yhNXvbTlt273EqO3kIggUy1e-uZso60eHwo/edit?gid=996600110#gid=996600110',
+  },
   { key: 'vivian', member: '비비안', endpoint: '/api/prison-manual-schedule?key=vivian', sourceUrl: '' },
   { key: 'amanemay', member: '김메이', endpoint: '/api/prison-manual-schedule?key=amanemay', sourceUrl: '' },
   { key: 'honoe1330', member: '이치유', endpoint: '/api/prison-manual-schedule?key=honoe1330', sourceUrl: '' },
