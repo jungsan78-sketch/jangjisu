169fdb98933fc35af6a68463f464c629280e3d01
jungsan78-sketch <jungsan78@gmail.com>
2026-08-29 00:35:52 +0900

    Expand fan archive main layout

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -29,7 +29,7 @@ export default function SiteFooter({ className = '' }) {
   return (
     <>
       <footer className={`px-5 pb-8 pt-12 text-center lg:px-8 ${className}`}>
-        <div className="mx-auto max-w-7xl border-t border-white/[0.06] pt-6">
+        <div className="mx-auto max-w-[1760px] border-t border-white/[0.06] pt-6">
           <p className="text-xs font-semibold tracking-[0.08em] text-white/35 sm:text-sm">
             ©2026 SANE. <span className="ml-1">All rights reserved.</span>
           </p>
